5a120ef737529cab7fa1065492b411952bb022d4
mganeko <m.ganeko@gmail.com>
2021-04-20 11:56:08 +0900

    add request info

--- mod.js
+++ mod.js
@@ -1,7 +1,16 @@
 addEventListener("fetch", (event) => {
+  // const json = JSON.stringify({
+  //   message: "Hello from Deno Deploy",
+  // });
+
   const json = JSON.stringify({
     message: "Hello from Deno Deploy",
+    url: event.request.url,
+    method: event.request.method,
+    proto: event.request.proto,
+    headers: Object.fromEntries(evnet.request.headers.entries())
   });
+
 
   const response = new Response(json, {
     headers: {
